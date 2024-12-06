7a765276a4e7b4e4ca9163489bd071cc7e0ede04
landdebbarma <landdebbarma5@gmail.com>
2024-12-06 18:58:46 +0530

    minor fix

--- Billing/index.php
+++ Billing/index.php
@@ -5,106 +5,112 @@ include('includes/nav.php');
 
 <div class="card position-absolute start-0 shadow bg-body-tertiary h-100 " style="width: 40%;">
 
-<div class="card mt-5 h-50 d-flex align-items-center">
-<div class="form-floating w-50">
-        <input type="text" id="sellerContact" class="form-control mt-3" placeholder="Customer Name">
-        <label for="sellerContact">Customer Name</label>
-      </div>
-<div class="form-floating w-50">
-        <input type="text" id="sellerContact" class="form-control mt-3" placeholder="Customer contact">
-        <label for="sellerContact">Customer Contact</label>
-      </div>
-      <div class="form-floating w-50">
-        <input type="text" id="sellerContact" class="form-control mt-3" placeholder="Items">
-        <label for="sellerContact">Items Name</label>
-      </div>
+    <div class="card mt-5 h-50 d-flex align-items-center">
+        <div class="form-floating w-50">
+            <input type="text" id="sellerContact" class="form-control mt-3" placeholder="Customer Name">
+            <label for="sellerContact">Customer Name</label>
+        </div>
+        <div class="form-floating w-50">
+            <input type="text" id="sellerContact" class="form-control mt-3" placeholder="Customer contact">
+            <label for="sellerContact">Customer Contact</label>
+        </div>
+        <div class="form-floating w-50">
+            <input type="text" id="sellerContact" class="form-control mt-3" placeholder="Items">
+            <label for="sellerContact">Items Name</label>
+        </div>
 
-      <div class="d-grid gap-3 d-md-block mt-3">
-      <button type="button" class="btn btn-outline-danger">Cancel</button>
-      <button type="button" class="btn btn-outline-success">Export</button>
+        <div class="d-grid gap-3 d-md-block mt-3">
+            <button type="button" class="btn btn-outline-danger">Cancel</button>
+            <button type="button" class="btn btn-outline-success">Export</button>
 
-</div>
+        </div>
 
-</div>
+    </div>
 
 </div>
 
 <div class="w-60 p-5 ml-5 shadow bg-body-tertiary position-absolute end-0 h-100">
-<div class="container text-center card">
-  <div class="row">
-    <div class="col-sm-5 col-md-6 fw-bold mt-3">CHOKELENG GRAPHIC
-        <div class="fw-normal mt-3 mb-3">
-            near don bosco agartala mark para<br>
-            dfkdkfgvgrdfkgvk<br>
-            email:ladbbduckdc<br>
-            ggst 77437247427e232753823755
+    <div class="container text-center card">
+        <div class="row">
+            <div class="col-sm-5 col-md-6 fw-bold mt-3">CHOKELENG GRAPHIC
+                <div class="fw-normal mt-3 mb-3">
+                    near don bosco agartala mark para<br>
+                    dfkdkfgvgrdfkgvk<br>
+                    email:ladbbduckdc<br>
+                    ggst 77437247427e232753823755
+                </div>
+            </div>
+            <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0 card position-relative mt-3 ">
+                <div class="card w-50 position-absolute top-0 start-0 fw-bold">invoice<br>36734337</div>
+                <div class="card w-50 position-absolute top-0 end-0 fw-bold">Date<br>12/12/12</div>
+
+                <div class="fw-normal mt-5 mb-10">
+                    near don bosco agartala mark para<br>
+                    dfkdkfgvgrdfkgvk<br>
+                    email:ladbbduckdc<br>
+                    ggst 77437247427e232753823755
+                </div>
+
+            </div>
         </div>
+
+        <table class="table mt-5">
+            <thead>
+                <tr>
+                    <th scope="col">SL.no</th>
+                    <th scope="col">Item Name</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">GST</th>
+                    <th scope="col">Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>gandu</td>
+                    <td>1</td>
+                    <td>15</td>
+                    <td>lk34hh67</td>
+                    <td>20</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>lipika</td>
+                    <td>bhabhi hain ha mara</td>
+                    <td>(not for seal) book by shafru</td>
+                    <td>shafru phone no</td>
+                    <td>out of range</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>shafru</td>
+                    <td>1</td>
+                    <td>12</td>
+                    <td>1hfh64gh</td>
+                    <td>15</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>shafru</td>
+                    <td>1</td>
+                    <td>12</td>
+                    <td>1hfh64gh</td>
+                    <td>15</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div class="card mb-5">
+            <div class="fw-bold p-2 g-col-6 position-absolute top-0 start-0">Invoice in words :</div>
+            <div class="fw-bold p-2 g-col-6 position-absolute  top-0 end-0">xgcgdgcgdwcfgeifguvfiuer</div>
+        </div>
+
+        <div class="card mb-5">
+            <p class="bank-details">bank details</p>
+        </div>
+
     </div>
-    <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0 card position-relative mt-3">
-        <div class="card w-50 position-absolute top-0 start-0 fw-bold">invoice<br>36734337</div>
-       
-        <div class="card w-50 position-absolute top-0 end-0 fw-bold">Date<br>12/12/12</div>
-       
-    </div>
-  </div>
-
-  <table class="table mt-5">
-  <thead>
-    <tr>
-      <th scope="col">SL.no</th>
-      <th scope="col">Item Name</th>
-      <th scope="col">Quantity</th>
-      <th scope="col">Price</th>
-      <th scope="col">GST</th>
-      <th scope="col">Amount</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>gandu</td>
-      <td>1</td>
-      <td>15</td>
-      <td>lk34hh67</td>
-      <td>20</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>lipika</td>
-      <td>bhabhi hain ha mara</td>
-      <td>(not for seal) book by shafru</td>
-      <td>shafru phone no</td>
-      <td>out of range</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>shafru</td>
-      <td>1</td>
-      <td>12</td>
-      <td>1hfh64gh</td>
-      <td>15</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>shafru</td>
-      <td>1</td>
-      <td>12</td>
-      <td>1hfh64gh</td>
-      <td>15</td>
-    </tr>
-  </tbody>
-</table>
-
-<div class="card mb-5">
-    <div class="fw-bold p-2 g-col-6 position-absolute top-0 start-0">Invoice in words :</div>
-    <div class="fw-bold p-2 g-col-6 position-absolute  top-0 end-0">xgcgdgcgdwcfgeifguvfiuer</div>
-</div>
-
-<div class="card mb-5">
-    <p class="bank-details">bank details</p>
-</div>
-
-</div>
 </div>
 
 <?php include('includes/footer.php') ?>
