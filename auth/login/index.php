8153b5a58af286e5f7320a3cc74f8221a17a0163
landdebbarma <landdebbarma5@gmail.com>
2025-02-05 14:27:18 +0530

    login and signup page update and some bug fix

--- auth/login/index.php
+++ auth/login/index.php
@@ -4,7 +4,45 @@ include('../../config/dbcon.php');
 ?>
 
 <!-- All Codes Should Be Written Here -->
-<div class="p-4">Test Function</div>
-<button class="btn btn-primary m-4">Test</button>
+<div class="card vh-100" style="max-width: 900vh; border-radius:30px">
+  <div class="row g-0">
+    <div class="card" style="width: 40%; position:absolute;border-radius:30px">
+      <img src="../img/hj.avif" class="img-fluid rounded-start vh-100" style="width: 100rem;border-radius:30px" alt="...">
+    </div>
+    <div class="col-md-8" style="position:relative;margin-left:40%;border-radius:30px">
+      <div class="card-body">
+        <h3 class="" style="padding-left:385px;margin-top:30px">Welcome Back</h3>
+        <h6 class="" style="padding-left:370px; margin-top:30px">Please login to your account</h6>
+
+        <!-- input field -->
+        <div class="form-floating mb-3" style="margin-left: 200px;">
+            <input type="text" class="form-control mt-5" style="width: 80vh;" id="givenCustomerName" name="name" placeholder="" required>
+            <label for="givenCustomerName">Email Address</label>
+          </div>
+
+          <div class="form-floating mb-3" style="margin-left: 200px;">
+            <input type="password" class="form-control mt-5" style="width: 80vh;" id="givenCustomerName" name="name" placeholder="" required>
+            <label for="givenCustomerName">Password</label>
+          </div>  
+
+          <div class=" mb-3" style="margin-left: 200px; position:absolute">   
+          <input type="checkbox"  id="btncheck1" autocomplete="off">
+          <label  for="btncheck1">Remember Me</label>
+          </div>
+          
+          <div class=" mb-3" style="margin-left: 92vh; color:red; position:relative"> 
+            <p>forgot Password</p>
+          </div>
+
+     
+          <button type="button" class="btn btn-success mt-5 p-2" style="width: 150px; margin-left: 200px;">Login</button>
+
+          <button type="button" class="btn btn-outline-success mt-5 p-2"style="width: 150px; margin-left: 250px;">Create Account</button>
+      
+
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include('../includes/footer.php'); ?>
--- auth/signup/index.php
+++ auth/signup/index.php
@@ -4,7 +4,48 @@ include('../../config/dbcon.php');
 ?>
 
 <!-- All Codes Should Be Written Here -->
-<div class="p-4">Test Function</div>
-<button class="btn btn-primary m-4">Test</button>
+<div class="card vh-100" style="max-width: 900vh;">
+  <div class="row g-0">
+   
+    <div class="col-md-8" style="position:relative;">
+      <div class="card-body">
+        <h3 class="card-title mt-5" style="margin-left: 150px;">Create An Account</h3>
+        <h6 class="card-title mt-4" style="margin-left: 150px;">All ready have an Account</h6>
+
+        <div class="form-floating mb-3" style="margin-left: 150px;position:relative">
+            <input type="text" class="form-control mt-5" style="width: 40vh;" id="givenCustomerName" name="name" placeholder="" required>
+            <label for="givenCustomerName">First Name</label>
+          </div>
+          <div class="form-floating mb-3" style="margin-left: 450px;position:absolute; margin-top:-75px">
+            <input type="text" class="form-control " style="width: 40vh;" id="givenCustomerName" name="name" placeholder="" required>
+            <label for="givenCustomerName">Last Name</label>
+          </div>
+
+          <div class="form-floating mb-3" style="margin-left: 150px;">
+            <input type="text" class="form-control mt-4" style="width: 83vh;" id="givenCustomerName" name="name" placeholder="" required>
+            <label for="givenCustomerName">Email Address Or phone number</label>
+          </div>
+
+          <div class="form-floating mb-3" style="margin-left: 150px;">
+            <input type="password" class="form-control mt-4" style="width: 83vh;" id="givenCustomerName" name="name" placeholder="" required>
+            <label for="givenCustomerName">Password</label>
+          </div> 
+
+          <div class=" mb-3" style="margin-left: 150px; position:absolute">   
+          <input type="checkbox"  id="btncheck1" autocomplete="off">
+          <label  for="btncheck1">I agree to the trams and condition</label>
+          </div>
+
+          <button type="button" class="btn btn-success mt-5 p-2" style="width: 83vh; margin-left: 150px;">Create account</button>
+     
+      </div>
+    </div>
+
+    <div class="card" style="width: 40%; position:absolute; border-radius:30px;margin-left:60%;">
+      <img src="../img/mm.avif" class="img-fluid rounded-start vh-100 rounded-5" style="border-radius:30px" alt="...">
+    </div>
+
+  </div>
+</div>
 
 <?php include('../includes/footer.php'); ?>
