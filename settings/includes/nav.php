7aca69c9582dfa9b682a0a8e1467c12ddfe33c81
Alam-Monir <monirrza0641@gmail.com>
2025-02-05 18:21:53 +0530

    settings nav added

---
+++ settings/includes/nav.php
@@ -0,0 +1,17 @@
+<div class="d-flex justify-content-between px-4 pt-4">
+    <div class="d-flex">
+        <a href="/idandbilling/" class="pt-2 px-2">
+            <button class="btn btn-outline-primary" type="button">
+                <i class="bi bi-house"></i> Home
+            </button>
+        </a>
+        <a href="/idandbilling/settings/" class=" px-2">
+            <i class="bi bi-gear" style="font-size: 2.1rem;"></i>
+        </a>
+        <h1 id="dynamicHeadLink">Settings</h1>
+    </div>
+    <a id="dynamicButtonLink" href="">
+        <button id="dynamicButton" class="btn btn-outline-primary m-2" type="button">Create New Staff</button>
+    </a>
+</div>
+<hr class="border border-secondary border-2 opacity-75" />
--- settings/index.php
+++ settings/index.php
@@ -1,5 +1,6 @@
 <?php
 include("includes/header.php");
+include("includes/nav.php");
 include("../config/dbcon.php");
 include ('../auth/auth_check_admin.php');
 ?>
