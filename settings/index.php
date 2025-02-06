dea20e04c8e4d6efb6c21d993b7dd5a7fb18d10f
landdebbarma <landdebbarma5@gmail.com>
2025-02-06 16:58:37 +0530

    settings update

--- settings/index.php
+++ settings/index.php
@@ -7,49 +7,50 @@ include ('../auth/auth_check_admin.php');
 
 <!-- left-box -->
 <div class="row">
-  <div class="col-sm-6 mb-3 mb-sm-0">
+<h5 class="modal-title fs-4" id="exampleModalLabel">Admin Details</h5>
+  <div class="col-sm-6 mb-3 mb-sm-0 w-25">
     <div class="card">
       <div class="card-body">
 
       <h1 class="modal-title fs-6" id="exampleModalLabel">Change User Name</h1>
-      <div class="form-floating w-50 position-relative">
+      <div class="form-floating w-80">
             <input type="text" id="Name" class="form-control mt-3" placeholder="Customer Name">
             <label for="text">Name</label>
         </div>
-        <div class="form-floating w-50 position-absolute  top-0 end-0 mt-5">
-            <input type="text" id="Name" class="form-control mt-2" placeholder="text">
+        <div class="form-floating w-80  top-0 end-0 mt-3">
+            <input type="text" id="Name" class="form-control" placeholder="text">
             <label for="text">Last Name</label>
         </div>
 
         <h1 class="modal-title fs-6 mt-4" id="exampleModalLabel">Change phone number</h1>
-      <div class="form-floating w-100">
+      <div class="form-floating w-80">
             <input type="number" id="number" class="form-control mt-3" placeholder="number">
             <label for="number">phone number</label>
         </div>
 
         <h1 class="modal-title fs-6 mt-3" id="exampleModalLabel">Change Email Address</h1>
-      <div class="form-floating w-100">
+      <div class="form-floating w-80">
             <input type="text" id="email" class="form-control mt-3" placeholder="email">
             <label for="email">Email</label>
         </div>
 
         <h1 class="modal-title fs-6 mt-4" id="exampleModalLabel">Change Password</h1>
-      <div class="form-floating w-100">
+      <div class="form-floating w-80">
             <input type="password" id="password" class="form-control mt-3" placeholder="password">
             <label for="password"> New Password</label>
         </div>
 
       
-      <div class="form-floating w-100">
+      <div class="form-floating w-80">
             <input type="password" id="password" class="form-control mt-3" placeholder="password">
             <label for="password">confirm Password</label>
         </div>
 
 
-      <div class="modal-footer justify-content-between mt-3">
-      <button type="button" class="btn btn-success w-100">Save</button>
-        <button type="button" class="btn btn-outline-danger w-100 mt-3" data-bs-dismiss="modal">Cancel</button>
-      </div>
+    
+      <button type="button" class="btn btn-success w-80 mt-3">Save</button>
+        <button type="button" class="btn btn-outline-danger w-80 mt-3" data-bs-dismiss="modal">Cancel</button>
+
 
 
 
@@ -58,7 +59,7 @@ include ('../auth/auth_check_admin.php');
   </div>
 
 <!-- right-box -->
-  <div class="col-sm-6">
+  <div class="col-sm-6 w-75">
     <div class="card">
       <div class="card-body vh-100">
       
@@ -68,6 +69,7 @@ include ('../auth/auth_check_admin.php');
       <th scope="col">SL.NO</th>
       <th scope="col">Name</th>
       <th scope="col">Contact</th>
+      <th scope="col">Email</th>
       <th scope="col">Address</th>
       <th scope="col">Manage</th>
     </tr>
@@ -79,6 +81,7 @@ include ('../auth/auth_check_admin.php');
       <td>1234567890</td>
       <td>T jirania</td>
       <td>*****</td>
+      <td><i class="bi bi-pen mx-1 edit-quantity-icon" style="cursor: pointer;"></i></td>
     </tr>
     <tr>
       <th scope="row">2</th>
@@ -86,6 +89,7 @@ include ('../auth/auth_check_admin.php');
       <td>244353636</td>
       <td>road 10</td>
       <td>*****</td>
+      <td><i class="bi bi-pen mx-1 edit-quantity-icon" style="cursor: pointer;"></i></td>
     </tr>
     <tr>
       <th scope="row">3</th>
@@ -93,6 +97,7 @@ include ('../auth/auth_check_admin.php');
       <td>4636346</td>
       <td>fhdfghdfg</td>
       <td>*****</td>
+      <td><i class="bi bi-pen mx-1 edit-quantity-icon" style="cursor: pointer;"></i></td>
     </tr>
   </tbody>
 </table>
