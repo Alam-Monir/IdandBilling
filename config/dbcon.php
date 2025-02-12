d61a7115794a55ae712a6f2840e070e8cdf4b04e
landdebbarma <landdebbarma5@gmail.com>
2025-02-12 20:57:19 +0530

    fix

--- config/dbcon.php
+++ config/dbcon.php
@@ -4,10 +4,16 @@ $db = "idandbilling";
 $user = "root";
 $pass = "";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode to exception
-} catch (PDOException $e) {
-    echo "Database connection failed: " . $e->getMessage();
-    exit;
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode to exception
+// } catch (PDOException $e) {
+//     echo "Database connection failed: " . $e->getMessage();
+//     exit;
+// }
+
+$con = mysqli_connect($servername, $username, $password,$db);
+if (!$con){
+   die("connection faild: " .mysqli_connect_error());
 }
+?>
