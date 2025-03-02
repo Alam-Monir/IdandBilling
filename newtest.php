50689f3740391f2254dabc3333781c1a9e5d3871
Alam-Monir <monirrza0641@gmail.com>
2025-03-02 17:05:29 +0530

    test file added

---
+++ newtest.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Chongkhlenggraphics</title>
+    <style>
+        .test {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            font-size: 50px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="test">Coming Soon</div>
+</body>
+
+</html>
